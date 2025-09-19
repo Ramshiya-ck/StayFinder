7558634eee8358ed8bc9479acd837104631e514e
ramshiya <ckramshiyaa@gmail.com>
2025-09-19 15:19:11 +0530

    updated..

--- api/v1/booking/views.py
+++ api/v1/booking/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import get_object_or_404
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 from api.v1.booking.serializers import *
 from booking.models import *
